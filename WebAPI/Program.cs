2f686ff96330b9bc46eff00b08a7fcc14c6f0400
Ricardo Luna Torres <ricardo.luna@zapotlan.gob>
2020-09-17 14:43:43 -0500

    Section 8- Class 59

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,43 +1,41 @@
+using System;
+using Dominio;
+
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Persistencia;
-using System;
 
-namespace WebAPI
-{
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			var hostserver = CreateHostBuilder(args).Build();
-			using (var ambiente = hostserver.Services.CreateScope())
-			{
+namespace WebAPI {
+	public class Program {
+		public static void Main (string[] args) {
+			var hostserver = CreateHostBuilder (args).Build ();
+			using (var ambiente = hostserver.Services.CreateScope ()) {
 				var services = ambiente.ServiceProvider;
-				try
-				{
-					var context = services.GetRequiredService<CursosOnlineContext>();
-					context.Database.Migrate();
-				}
-				catch (Exception e)
-				{
-					var loggin = services.GetRequiredService<ILogger<Program>>();
-					loggin.LogError(e, "Ocurrió un error en la migración");
+				try {
+					var userManager = services.GetRequiredService<UserManager<Usuario>> ();
+					var context = services.GetRequiredService<CursosOnlineContext> ();
+					context.Database.Migrate ();
+					DataPrueba.InsertarData (context, userManager).Wait();
+					
+				} catch (Exception e) {
+					var loggin = services.GetRequiredService<ILogger<Program>> ();
+					loggin.LogError (e, "Ocurrió un error en la migración");
 				}
 
 			}
-			hostserver.Run();
+			hostserver.Run ();
 			//   CreateHostBuilder(args).Build().Run();
 
 		}
 
-		public static IHostBuilder CreateHostBuilder(string[] args) =>
-			Host.CreateDefaultBuilder(args)
-				.ConfigureWebHostDefaults(webBuilder =>
-				{
-					webBuilder.UseStartup<Startup>();
-				});
+		public static IHostBuilder CreateHostBuilder (string[] args) =>
+			Host.CreateDefaultBuilder (args)
+			.ConfigureWebHostDefaults (webBuilder => {
+				webBuilder.UseStartup<Startup> ();
+			});
 	}
 }
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,58 +1,60 @@
 using Aplicacion.Cursos;
+using Dominio;
 using FluentValidation.AspNetCore;
 using MediatR;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using Persistencia;
 using WebAPI.Middleware;
 
-namespace WebAPI
-{
-	public class Startup
-	{
-		public Startup(IConfiguration configuration)
-		{
+namespace WebAPI {
+	public class Startup {
+		public Startup (IConfiguration configuration) {
 			Configuration = configuration;
 		}
 
 		public IConfiguration Configuration { get; }
 
 		// This method gets called by the runtime. Use this method to add services to the container.
-		public void ConfigureServices(IServiceCollection services)
-		{
+		public void ConfigureServices (IServiceCollection services) {
 			//Conexión donde se toma la base de datos
-			services.AddDbContext<CursosOnlineContext>(opt =>
-			{
-				opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+			services.AddDbContext<CursosOnlineContext> (opt => {
+				opt.UseSqlServer (Configuration.GetConnectionString ("DefaultConnection"));
 			});
-			services.AddMediatR(typeof(Consulta.Manejador).Assembly);
-			services.AddControllers().AddFluentValidation(
-				cfg => cfg.RegisterValidatorsFromAssemblyContaining<Nuevo>()
-				);
+			services.AddMediatR (typeof (Consulta.Manejador).Assembly);
+			services.AddControllers ().AddFluentValidation (
+				cfg => cfg.RegisterValidatorsFromAssemblyContaining<Nuevo> ()
+			);
+			var builder = services.AddIdentityCore<Usuario>();
+			var identityBuilder = new IdentityBuilder (builder.UserType, builder.Services);
+			identityBuilder.AddEntityFrameworkStores<CursosOnlineContext> ();
+			identityBuilder.AddSignInManager<SignInManager<Usuario>> ();
+			services.TryAddSingleton<ISystemClock, SystemClock>();
+			//services.TryAddSingleton<ISystemClock, SystemClock>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-		{
-			app.UseMiddleware<ManejadorErrorMiddleware>();
-			if (env.IsDevelopment())
-			{
+		public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+			app.UseMiddleware<ManejadorErrorMiddleware> ();
+			if (env.IsDevelopment ()) {
 				// app.UseDeveloperExceptionPage();
 			}
 
 			//app.UseHttpsRedirection();
 
-			app.UseRouting();
+			app.UseRouting ();
 
-			app.UseAuthorization();
+			app.UseAuthorization ();
 
-			app.UseEndpoints(endpoints =>
-			{
-				endpoints.MapControllers();
+			app.UseEndpoints (endpoints => {
+				endpoints.MapControllers ();
 			});
 		}
 	}
